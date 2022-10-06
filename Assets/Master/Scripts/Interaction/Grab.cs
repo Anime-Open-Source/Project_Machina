22aa3e3c2ee213298296bbfc5a1ad830fe3cd707
Arsa <umarsetyawan10@gmail.com>
2022-10-06 08:05:49 +0700

    Feat [US] : Added Collider Base Grab System
    Added Grab System that use Collider instead of Ray, also added the option to change Grab System in Interaction Manager

--- Assets/Master/Scripts/Interaction/Grab.cs
+++ Assets/Master/Scripts/Interaction/Grab.cs
@@ -11,21 +11,33 @@ public class Grab : MonoBehaviour
     [SerializeField] private InteractionManager _InteractionManager;
     [Space(5)]
     [SerializeField] private bool _IsHolding;
+    [SerializeField] private bool _IsClicked;
 
     private float _Range;
     private bool _ClickGrab;
+    private GameObject _GrabedObject;
     private IGrabable c_Target;
 
     private void Start()
     {
         _Range = _InteractionManager.Range;
         _ClickGrab = _InteractionManager.ClickGrab;
+        _IsClicked = false;
     }
 
     private void Update()
     {
-        if (!_GrabedObject.activeSelf)
-            _IsHolding = false;
+        if (_GrabedObject != null)
+        {
+            if (!_GrabedObject.activeSelf)
+                Detached(c_Target);
+        }
+
+        if (_InteractionManager.UseRayGrab)
+            this.GetComponent<Collider>().enabled = false;
+        else
+            this.GetComponent<Collider>().enabled = true;
+
     }
 
     #region Grab Functions
@@ -33,34 +45,80 @@ public class Grab : MonoBehaviour
     #region Raybase Grab Function
     public void RayGrab(InputAction.CallbackContext context)
     {
-        if (!_IsHolding)
+        if (!_IsHolding && _InteractionManager.UseRayGrab)
         {
             c_Target = null;
             Physics.Raycast(transform.position, transform.position + transform.forward * _Range, out RaycastHit hit);
-            if (hit.transform.CompareTag("grab"))
+            if (hit.collider.enabled && hit.collider.CompareTag("grab"))
             {
-                _GrabedObject = hit.transform.gameObject;
-                c_Target = _GrabedObject.GetComponent<IGrabable>();
+
+                _GrabedObject = hit.collider.gameObject;
+
+                if (_GrabedObject.GetComponent<IGrabable>() != null)
+                    c_Target = _GrabedObject.GetComponent<IGrabable>();
+                else
+                    _GrabedObject.SetActive(false);
+
                 GrabItem(c_Target);
+                
             }
             
         }
 
 
-        if (!context.ReadValueAsButton() && !_ClickGrab)
+        if (!context.ReadValueAsButton())
         {
-            Debug.Log("Detached");
-            Detached(c_Target);
-        }
-
-        if (context.performed && _ClickGrab)
-        {
-            Debug.Log("Detached");
-            Detached(c_Target);
+            if (!_ClickGrab)
+            {
+                Debug.Log("Detached");
+                Detached(c_Target);
+            }
+            else
+            {
+                if (_IsClicked)
+                {
+                    Debug.Log("Detached");
+                    Detached(c_Target);
+                }
+                else
+                    _IsClicked = true;
+            }
+            
         }
 
     }
     #endregion
+
+
+    #region Colliderbase Grab Function
+    private void OnTriggerEnter(Collider other)
+    {
+        if (_InteractionManager.UseRayGrab || _GrabedObject != null)
+            return;
+        else
+        {
+            if (_GrabedObject == other)
+                Detached(c_Target);
+
+            if (other.enabled && other.CompareTag("grab"))
+            {
+                _GrabedObject = other.gameObject;
+
+                if (_GrabedObject.GetComponent<IGrabable>() != null)
+                    c_Target = _GrabedObject.GetComponent<IGrabable>();
+                else
+                    _GrabedObject.SetActive(false);
+
+                _IsClicked = true;
+
+                GrabItem(c_Target);
+            }
+        }
+        
+    }
+    #endregion
+
+
 
     #region Grab and Detached
     private void GrabItem(IGrabable Target)
@@ -73,10 +131,19 @@ public class Grab : MonoBehaviour
     {
         Debug.Log(string.Format("Detached from : {0}", this.name));
         _IsHolding = false;
+        _IsClicked = false;
         _GrabedObject = null;
         Target.Detached(this.gameObject);
     }
     #endregion
 
     #endregion
+
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + transform.forward * 5f);
+    }
 }
--- Assets/Master/Scripts/Interaction/InteractionManager.cs
+++ Assets/Master/Scripts/Interaction/InteractionManager.cs
@@ -13,9 +13,12 @@ public class InteractionManager : MonoBehaviour
     [Tooltip("If enabled will change the grab from hold to click")]
     [SerializeField] private bool _ClickGrab;
 
+    [SerializeField] private bool _UseRayGrab;
+
 
     public float SmoothTime { get { return _SmoothTime; } private set { } }
     public float Range { get { return _Range; } private set { } }
     public bool ClickGrab { get { return _ClickGrab; } private set { } }
+    public bool UseRayGrab { get { return _UseRayGrab; } set { _UseRayGrab = value; } }
 
 }
