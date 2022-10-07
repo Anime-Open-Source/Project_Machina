37aad8ed0328d2a5ab75fbbf27670013a918ce6b
Arsa <umarsetyawan10@gmail.com>
2022-10-07 12:53:37 +0700

    Fix [US] : Fix the BowString Bug
    Fix the BowString Bug where if you pull the bowstring and then move the bow around the string will reset

--- Assets/Master/Scripts/Weapons/Bow/BowString.cs
+++ Assets/Master/Scripts/Weapons/Bow/BowString.cs
@@ -7,7 +7,7 @@ public class BowString : MonoBehaviour, IGrabable
     #region Interface Implementation
     public void Detached(GameObject Hand)
     {
-
+        
         _HandTransform = null;
         _IsGrabbed = false;
     }
@@ -15,12 +15,26 @@ public class BowString : MonoBehaviour, IGrabable
     public void Grabed(GameObject Hand)
     {
         
-
         _HandTransform = Hand.transform;
         _IsGrabbed = true;
 
     }
     #endregion
+
+    private void OnDisable()
+    {
+        _ParentBow.Charge = 0;
+        _DotProduct = 0f;
+        transform.position = _PullStartPos;
+
+        _PullStartPos = Vector3.zero;
+        _LocalStartPos = Vector3.zero;
+    }
+
+    private void OnEnable()
+    {
+        _LocalStartPos = transform.localPosition;
+    }
 
     private Transform _HandTransform;
     
@@ -35,22 +49,6 @@ public class BowString : MonoBehaviour, IGrabable
     private float _DotProduct;
     private float _SmoothTime;
 
-
-    private void OnEnable()
-    {
-        if (_LocalStartPos == Vector3.zero || _LocalStartPos != transform.localPosition)
-            _LocalStartPos = transform.localPosition;
-
-        if (_LocalStartPos == transform.localPosition)
-            _PullStartPos = transform.position;
-    }
-
-    private void OnDisable()
-    {
-        _LocalStartPos = Vector3.zero;
-        _PullStartPos = Vector3.zero;
-    }
-
     private void Start()
     {
         if (!this.gameObject.activeSelf)
@@ -63,20 +61,24 @@ public class BowString : MonoBehaviour, IGrabable
     {
         _SmoothTime = _ParentBow.SmoothTime;
 
+        if (_LocalStartPos == transform.localPosition)
+            _PullStartPos = transform.position;
+
+        //Check if hand is not null
         if (_HandTransform)
             transform.position = _HandTransform.position;
 
-        if (!_IsGrabbed && Vector3.Distance(transform.localPosition, _PullStartPos) >= 0f)
-        {
+        //Move Back To start if not grabed
+        if (!_IsGrabbed && Vector3.Distance(transform.position, _PullStartPos) >= 0f)
             transform.localPosition = Vector3.SmoothDamp(transform.localPosition, _LocalStartPos, ref _Velocity, _SmoothTime);
-        }
+
+        Vector3 Forward = _ParentBow.transform.TransformDirection(Vector3.forward);
+        Vector3 Other = transform.position - _ParentBow.transform.position;
+        _DotProduct = Vector3.Dot(Forward, Other); ;
 
 
-        _DotProduct = Vector3.Dot(transform.forward, _PullStartPos - transform.position);
-
-
-        if (_DotProduct >= _ParentBow.MinimalPull)
-            _ParentBow.Charge = _DotProduct;
+        if (_DotProduct <= _ParentBow.MinimalPull)
+            _ParentBow.Charge = -_DotProduct;
         else
             _ParentBow.Charge = 0;
 
