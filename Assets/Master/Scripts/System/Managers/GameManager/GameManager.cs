f6705e341ee4dd01aac24c8f73bea6f4fb282ec3
Arsa <umarsetyawan10@gmail.com>
2023-02-25 11:14:43 +0700

    Fix [US] : Fix the Game Manager Pause function
    Update the Game Manager Pause function, now the Game Manager will check if the game is already in progress or not, this will fix bug where the rest timer is shown before the game start.

--- Assets/Master/Scripts/System/Managers/GameManager/GameManager.cs
+++ Assets/Master/Scripts/System/Managers/GameManager/GameManager.cs
@@ -5,22 +5,39 @@ using UnityEngine.Events;
 
 public class GameManager : SingletonGeneric<GameManager>
 {
+
+    private bool _isGameInProgress;
+
+    private int _score;
+
+    public int PlayerScore { get { return _score; } }
+
     private bool _IsPaused;
+
+    public UnityEvent OnGamePause;
+    public UnityEvent OnGameResume;
+
+    public bool IsGameInProgress { get { return _isGameInProgress; } set { _isGameInProgress = value; } }
 
     public void PauseGame() 
     {
 
-        if (_IsPaused)
+
+        if(_IsPaused && _isGameInProgress)
         {
             Time.timeScale = 1.0f;
+            OnGameResume.Invoke();
             _IsPaused = false;
         }
 
-        else
+        if(!_IsPaused && _isGameInProgress)
         {
+            OnGamePause.Invoke();
             Time.timeScale = 0f;
             _IsPaused = true;
         }
+
+
             
     }
 
